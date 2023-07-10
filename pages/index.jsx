acfcdec27756bf2adbfcf048dae00be3b64ba6ad
EuMotta <hyperxmotta@hotmail.com>
2023-07-10 20:23:53 -0300

    ajuste de identação v2

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import Layout from '@/components/Layout';
 import Hero from './sections/Home/Hero';
 import Services from './sections/Home/Services';
@@ -6,13 +7,12 @@ import PersonalUse from './sections/Home/PersonalUse';
 import Separator from './sections/Home/Separator';
 import BusinessUse from './sections/Home/BusinessUse';
 import Plans from './sections/Home/Plans';
-import Link from 'next/link';
 
 const index = () => (
   <Layout title="inicio">
-    
+
     <Hero />
-    <Link className='btn mt-10' href={'http://localhost:3000/car/64ac103a999acfe45185e219'}>Ir ver carro</Link>
+    <Link className="btn mt-10" href="http://localhost:3000/car/64ac103a999acfe45185e219">Ir ver carro</Link>
     <Services />
     <PersonalUse />
     <Separator />
